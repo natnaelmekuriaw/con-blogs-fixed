3d82ba17daf808102b6c82be98b3179c887e5a0d
natnael <natnael@ethiojobs.net>
2023-08-20 20:37:37 +0300

    reset back to simple axios fetching

--- ui/src/features/components/blog/Home.tsx
+++ ui/src/features/components/blog/Home.tsx
@@ -1,41 +1,29 @@
 import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import { getBlogs } from "../../actions/blogService";
-import { useQuery, useMutation } from "@tanstack/react-query";
 
 import { Blog } from "../../types";
 
 const Home = () => {
-  const blogsQuery = useQuery({
-    queryKey: ["blogs"],
-    queryFn: getBlogs,
-  });
-
   const [blogs, setBlogs] = useState<Blog[] | []>([]);
   const isLoading = false;
   const error = "";
   const titles = ["Blogs", "My Blogs"];
 
-  //   const getAllBlogs = async () => {
-  //     const allBlogs = await getBlogs();
-  //     setBlogs(allBlogs);
-  //     console.log("we have", allBlogs);
-  //   };
-  //   useEffect(() => {
-  //     getAllBlogs();
-  //   }, []);
-
-  if (blogsQuery.isLoading) {
-    return <h1>Fetching blogs...</h1>;
-  }
+  const getAllBlogs = async () => {
+    const allBlogs = await getBlogs();
+    setBlogs(allBlogs);
+    console.log("we have", allBlogs);
+  };
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
-      {blogsQuery.data && (
-        <BlogList blogs={blogsQuery.data} title={titles[0]} />
-      )}
+      {blogs && <BlogList blogs={blogs} title={titles[0]} />}
     </div>
   );
 };
