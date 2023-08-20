c2b231c95be1f88d0f15964dca310c1a4e221d8d
natnael <natnael@ethiojobs.net>
2023-08-20 20:47:38 +0300

    install and setup react-query and react-query-devtools

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,8 +4,10 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Home from "./features/components/blog/Home";
@@ -15,24 +17,29 @@ import BlogDetail from "./features/components/blog/BlogDetail";
 import Create from "./features/components/blog/Create";
 import PageNotFound from "./features/components/blog/PageNotFound";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <ToastContainer />
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/create" element={<Create />} />
-            <Route path="blogs/:id/*" element={<BlogDetail />} />
-            <Route path="/home" element={<Navigate to="/" />} />
-            <Route path="/test" element={<p>Test page</p>} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <ToastContainer />
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/create" element={<Create />} />
+              <Route path="blogs/:id/*" element={<BlogDetail />} />
+              <Route path="/home" element={<Navigate to="/" />} />
+              <Route path="/test" element={<p>Test page</p>} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
